cbbee44003cb6c047216cd8aab0c34c71ed7d5da
yoramzarai <yoram.zarai@gmail.com>
2024-11-21 23:56:33 +0200

    preparing for updated version with access modes.

--- src/geneanot/__init__.py
+++ src/geneanot/__init__.py
@@ -1,5 +1,7 @@
 
 # from geneanot.Gene_annotation import *
 # from geneanot.annotation_file_utils import *
-from .Gene_annotation import *
+
+#from .Gene_annotation import *
+from .Gene_annotation_V2 import *
 from .annotation_file_utils import *
